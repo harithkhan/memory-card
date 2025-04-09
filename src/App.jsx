30ccc593efa03c2a5164a9917822f7269ae94c38
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-04-09 16:34:16 +0800

    feat: add basic Main component + game instructions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 
 function App() {
     return (
-        <Header />
-    )
+        <>
+            <Header />
+            <Main />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,12 @@
+import "../../styles/Main.css";
+
+export default function Main() {
+    return (
+        <main>
+            <h3 className="instructions">
+                Click on unique character to accumulate score. Duplicates will
+                reset your progress.
+            </h3>
+        </main>
+    );
+}
