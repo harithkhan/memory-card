940e57a8abe73722c2923a62119f5f43e9bf8efa
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-04-09 15:32:36 +0800

    feat: add basic elements with styling of header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,11 +4,21 @@ import "../../styles/Header.css";
 export default function Header() {
     return (
         <header>
-            <img
-                src={headerIcon}
-                className="header-icon"
-                alt="Picture of Spongebob"
-            />
+            <div className="header-left">
+                <img
+                    src={headerIcon}
+                    className="header-icon"
+                    alt="Picture of Spongebob"
+                />
+                <div className="title-container">
+                    <h1 className="title-one">Spongebob Characters</h1>
+                    <h1 className="title-two">Memory Game</h1>
+                </div>
+            </div>
+            <div className="score-container">
+                <h2>Score: 0</h2>
+                <h2>Highscore: 0</h2>
+            </div>
         </header>
     );
 }
